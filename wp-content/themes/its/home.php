c33030f14f04b87f46ab8080660def1cddb1c506
Fabien Raymondaud <fabien.raymondaud@gmail.com>
2013-04-25 09:27:37 +0200

    Remontée home regards d'aujourd'hui
    Dynamisation du bloc de remontée Regards d'aujourd'hui sur la home.
    Retourne un article aléatoire associé au mot clé Regars d'aujourd'hui

--- wp-content/themes/its/home.php
+++ wp-content/themes/its/home.php
@@ -38,7 +38,7 @@
                 </div>
                 <article class="pl3 col">
                     <h3 class="very_biggest mb0 titre"><?php echo get_the_title($remontee_its->ID);?></h3>
-                    <h4 class="normal mt0 mb1"><?php the_field('date_article', $remontee_its->ID); ?> <?php the_field('auteur_article', $remontee_its->ID); ?></h4>
+                    <h4 class="normal mt0 mb1"><?php the_field('date_article', $remontee_its->ID); ?><span><?php the_field('auteur_article', $remontee_its->ID); ?></span></h4>
                     <div class="pb1 mb1">
                         <p class="small mb0"><?php echo $remontee_its->post_content;?></p>
                     </div>
@@ -51,17 +51,18 @@
             <h2 class="smaller"><span></span>Regards d'aujourd'hui</h2>
             <div class="row">
                 <article class="col">
-                    <h3 class="very_biggest mb0 titre">Défendons la laïcité</h3>
-                    <h4 class="normal mt0 mb1">Juin 1960 - Auteur</h4>
-                    <div class="pb2 mb1">
-                        <p class="small mb0">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore 
-                        eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    </div>
-                    <a href="#" class="small suite"><span>lire la suite</span></a>
+					<?php 
+                        $my_query = new WP_Query( array( 'post_type' => 'post', 'tag'=>'regards-d-aujourd-hui', 'orderby' => 'rand', 'posts_per_page'=>1));
+                        while( $my_query->have_posts() ) : $my_query->the_post();?>
+                            <h3 class="very_biggest mb0 titre"><?php the_title();?></h3>
+                            <h4 class="normal mt0 mb1"><?php the_field('date_article');?><span><?php the_field('auteur_article');?></span></h4>
+                            <div class="pb2 mb1 small">
+		                        <?php the_content();?>
+		                    </div>
+		                    <a href="<?php the_permalink(); ?>" class="small suite"><span>lire la suite</span></a>
+                        <?php endwhile;
+                    ?>
                 </article>
-
-
                 <section id="meme_theme" class="pl3 col">
                     <h3 class="small mb0">Archives sur le même thème</h3>
                     <h4 class="very_smaller mb1">Laïcité, éducation</h4>
