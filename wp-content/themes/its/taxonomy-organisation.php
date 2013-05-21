674c9442270ff181ffd44d93b5d42ee9fb6f3e21
Fabien Raymondaud <fabien.raymondaud@gmail.com>
2013-05-21 18:02:02 +0200

    Revert "Revert "Début de l'amélioration slider pages catégories/organisations""
    This reverts commit e6cd4ce246c7249724b8e4c351f6580a38bfa15f.

--- wp-content/themes/its/taxonomy-organisation.php
+++ wp-content/themes/its/taxonomy-organisation.php
@@ -35,8 +35,8 @@
 			}
 		?>
 		<?php
-			if(get_query_var('organisation')=="psu-60-90" || get_query_var('organisation')=="esu-60-71" || get_query_var('organisation')=="unef-60-71"){
-				if(get_query_var('organisation')=="psu-60-90"){
+			if(get_query_var('organisation')=="psu" || get_query_var('organisation')=="esu" || get_query_var('organisation')=="unef"){
+				if(get_query_var('organisation')=="psu"){
 		?>
 					<ul id="navigation_curseur" class="large mb1">
 		        		<li id="curseur_large"></li>
@@ -55,40 +55,40 @@
 		?>
 				<section id="frise" class="normal mt2 mb1 large">
 					<ul class="row pl3">
-						<li class="col" id="annee_1960"><a href="?annee=1960'; ?>">1960</a></li>
-						<li class="col" id="annee_1961"><a href="?annee=1961'; ?>">1961</a></li>
-						<li class="col" id="annee_1962"><a href="?annee=1962'; ?>">1962</a></li>
-						<li class="col" id="annee_1963"><a href="?annee=1963'; ?>">1963</a></li>
-						<li class="col" id="annee_1964"><a href="?annee=1964'; ?>">1964</a></li>
-						<li class="col" id="annee_1965"><a href="?annee=1965'; ?>">1965</a></li>
-						<li class="col" id="annee_1966"><a href="?annee=1966'; ?>">1966</a></li>
-						<li class="col" id="annee_1967"><a href="?annee=1967'; ?>">1967</a></li>
-						<li class="col" id="annee_1968"><a href="?annee=1968'; ?>">1968</a></li>
-						<li class="col" id="annee_1969"><a href="?annee=1969'; ?>">1969</a></li>
-						<li class="col" id="annee_1970"><a href="?annee=1970'; ?>">1970</a></li>
-						<li class="col" id="annee_1971"><a href="?annee=1971'; ?>">1971</a></li>
+						<li class="col" id="annee_1960"><a href="?annee=1960">1960</a></li>
+						<li class="col" id="annee_1961"><a href="?annee=1961">1961</a></li>
+						<li class="col" id="annee_1962"><a href="?annee=1962">1962</a></li>
+						<li class="col" id="annee_1963"><a href="?annee=1963">1963</a></li>
+						<li class="col" id="annee_1964"><a href="?annee=1964">1964</a></li>
+						<li class="col" id="annee_1965"><a href="?annee=1965">1965</a></li>
+						<li class="col" id="annee_1966"><a href="?annee=1966">1966</a></li>
+						<li class="col" id="annee_1967"><a href="?annee=1967">1967</a></li>
+						<li class="col" id="annee_1968"><a href="?annee=1968">1968</a></li>
+						<li class="col" id="annee_1969"><a href="?annee=1969">1969</a></li>
+						<li class="col" id="annee_1970"><a href="?annee=1970">1970</a></li>
+						<li class="col" id="annee_1971"><a href="?annee=1971">1971</a></li>
 		<?php
-						if(get_query_var('organisation')=="psu-60-90"){
+						if(get_query_var('organisation')=="psu"){
 		?>
-							<li class="col" id="annee_1972"><a href="?annee=1972'; ?>">1972</a></li>
-							<li class="col" id="annee_1973"><a href="?annee=1973'; ?>">1973</a></li>
-							<li class="col" id="annee_1974"><a href="?annee=1974'; ?>">1974</a></li>
-							<li class="col" id="annee_1975"><a href="?annee=1975'; ?>">1975</a></li>
-							<li class="col" id="annee_1976"><a href="?annee=1976'; ?>">1976</a></li>
-							<li class="col" id="annee_1977"><a href="?annee=1977'; ?>">1977</a></li>
-							<li class="col" id="annee_1978"><a href="?annee=1978'; ?>">1978</a></li>
-							<li class="col" id="annee_1979"><a href="?annee=1979'; ?>">1979</a></li>
-							<li class="col" id="annee_1980"><a href="?annee=1980'; ?>">1980</a></li>
-							<li class="col" id="annee_1981"><a href="?annee=1981'; ?>">1981</a></li>
-							<li class="col" id="annee_1982"><a href="?annee=1982'; ?>">1982</a></li>
-							<li class="col" id="annee_1983"><a href="?annee=1983'; ?>">1983</a></li>
-							<li class="col" id="annee_1984"><a href="?annee=1984'; ?>">1984</a></li>
-							<li class="col" id="annee_1985"><a href="?annee=1985'; ?>">1985</a></li>
-							<li class="col" id="annee_1986"><a href="?annee=1986'; ?>">1986</a></li>
-							<li class="col" id="annee_1987"><a href="?annee=1987'; ?>">1987</a></li>
-							<li class="col" id="annee_1988"><a href="?annee=1988'; ?>">1988</a></li>
-							<li class="col" id="annee_1989"><a href="?annee=1989'; ?>">1989</a></li>
-							<li class="col" id="annee_1990"><a href="?annee=1990'; ?>">1990</a></li>
+							<li class="col" id="annee_1972"><a href="?annee=1972">1972</a></li>
+							<li class="col" id="annee_1973"><a href="?annee=1973">1973</a></li>
+							<li class="col" id="annee_1974"><a href="?annee=1974">1974</a></li>
+							<li class="col" id="annee_1975"><a href="?annee=1975">1975</a></li>
+							<li class="col" id="annee_1976"><a href="?annee=1976">1976</a></li>
+							<li class="col" id="annee_1977"><a href="?annee=1977">1977</a></li>
+							<li class="col" id="annee_1978"><a href="?annee=1978">1978</a></li>
+							<li class="col" id="annee_1979"><a href="?annee=1979">1979</a></li>
+							<li class="col" id="annee_1980"><a href="?annee=1980">1980</a></li>
+							<li class="col" id="annee_1981"><a href="?annee=1981">1981</a></li>
+							<li class="col" id="annee_1982"><a href="?annee=1982">1982</a></li>
+							<li class="col" id="annee_1983"><a href="?annee=1983">1983</a></li>
+							<li class="col" id="annee_1984"><a href="?annee=1984">1984</a></li>
+							<li class="col" id="annee_1985"><a href="?annee=1985">1985</a></li>
+							<li class="col" id="annee_1986"><a href="?annee=1986">1986</a></li>
+							<li class="col" id="annee_1987"><a href="?annee=1987">1987</a></li>
+							<li class="col" id="annee_1988"><a href="?annee=1988">1988</a></li>
+							<li class="col" id="annee_1989"><a href="?annee=1989">1989</a></li>
+							<li class="col" id="annee_1990"><a href="?annee=1990">1990</a></li>
 		<?php
 						}
 		?>
