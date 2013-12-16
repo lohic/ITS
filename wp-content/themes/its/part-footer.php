2dad7a0f6dca218791d906db1c4cb090ecb82360
Loïc Horellou <loic@formidable-studio.net>
2013-12-16 16:07:18 +0100

    update footer

--- wp-content/themes/its/part-footer.php
+++ wp-content/themes/its/part-footer.php
@@ -4,10 +4,18 @@
         <footer class="row pt2 pb2">
         	<?php wp_nav_menu( array('menu'=>'Première colonne footer', 'container' => 'false', 'menu_id' => 'menu_footer_1', 'menu_class' => 'pl3 little_small col'));?>
         	<?php wp_nav_menu( array('menu'=>'Deuxième colonne footer', 'container' => 'false', 'menu_id' => 'menu_footer_2', 'menu_class' => 'little_small col'));?>
+        	<?php wp_nav_menu( array('menu'=>'Troisième colonne footer', 'container' => 'false', 'menu_id' => 'menu_footer_3', 'menu_class' => 'little_small col'));?>
         	<div>
-	        	<a href="<?php the_field('url_facebook', 'option'); ?>" id="facebook" title="facebook" target="_blank"><img src="<?php bloginfo( 'template_url' ); ?>/img/icn_facebook.png" alt="facebook"/></a>
-	        	<a href="<?php the_field('url_twitter', 'option'); ?>" id="twitter" title="twitter" target="_blank"><img src="<?php bloginfo( 'template_url' ); ?>/img/icn_twitter.png" alt="twitter"/></a>
-	        	<?php wp_nav_menu( array('menu'=>'Troisième colonne footer', 'container' => 'false', 'menu_id' => 'menu_footer_3', 'menu_class' => 'little_small col'));?>
+                <a href="<?php bloginfo('rss2_url'); ?>" id="rss" title="ITS RSS" target="_blank">
+                    <img src="<?php bloginfo( 'template_url' ); ?>/img/icn_rss.png" alt="RSS"/>
+                </a>
+	        	<a href="<?php the_field('url_facebook', 'option'); ?>" id="facebook" title="facebook" target="_blank">
+                    <img src="<?php bloginfo( 'template_url' ); ?>/img/icn_facebook.png" alt="facebook"/>
+                </a>
+	        	<a href="<?php the_field('url_twitter', 'option'); ?>" id="twitter" title="twitter" target="_blank">
+                    <img src="<?php bloginfo( 'template_url' ); ?>/img/icn_twitter.png" alt="twitter"/>
+                </a>
+	        	<?php wp_nav_menu( array('menu'=>'Quatrième colonne footer', 'container' => 'false', 'menu_id' => 'menu_footer_4', 'menu_class' => 'little_small col'));?>
 	        </div>    
 	        <section id="nuage" class="pr3 smallest col">
                 <?php 
