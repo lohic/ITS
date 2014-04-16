631b50424d9759d6bbb06151ebc656ee4a6d59ff
Loïc Horellou <loic@formidable-studio.net>
2014-04-16 12:22:59 +0200

    update function.php -> channel vimeo

--- wp-content/themes/its/functions.php
+++ wp-content/themes/its/functions.php
@@ -523,7 +523,9 @@ add_shortcode( 'fondsvideo', 'fondsvideo_shortcode' );
 function fondsvideo_shortcode( $atts ) {
 	extract( shortcode_atts( array(
 		'nbr' => 12,
-		'channel' => null
+		'channel' => null,
+		'notitle' => null,
+		'nodesc'  => null,
 	), $atts ) );
 
 	$url = 'https://vimeo.com/itsats/badgeo/?script=1&badge_layout=horizontal&badge_quantity='.$nbr.'&badge_size=160&badge_stream=uploaded&show_titles=yes';
@@ -535,35 +537,40 @@ function fondsvideo_shortcode( $atts ) {
 
 		$channelID = $channelInfo->id;
 
-		$url = 'https://vimeo.com/itsats/badgeo/?script=1&badge_layout=horizontal&badge_quantity='.$nbr.'&badge_size=160&badge_stream=channel&show_titles=no&badge_channel='.$channelID;
+		$url = 'https://vimeo.com/itsats/badgeo/?script=1&badge_layout=horizontal&badge_quantity='.$nbr.'&badge_size=160&badge_stream=channel&show_titles=yes&badge_channel='.$channelID;
 	}
 
 	//https://vimeo.com/api/v2/channel/psuguerredalgerie/info.json
 
 	$retour = '<style id="badge-styles">
         /* You can modify these CSS styles */
-        .vimeoBadge { margin: 0; padding: 0; font: normal 11px verdana,sans-serif; }
+        .vimeoBadge { margin: 0; padding: 0; }
         .vimeoBadge img { border: 0; }
-        .vimeoBadge a, .vimeoBadge a:link, .vimeoBadge a:visited, .vimeoBadge a:active { color: #ff3333; text-decoration: none; cursor: pointer; }
+        .vimeoBadge a, .vimeoBadge a:link, .vimeoBadge a:visited, .vimeoBadge a:active { color: #000; text-decoration: none; cursor: pointer; }
         .vimeoBadge a:hover { color:#188e7c; }
         .vimeoBadge #vimeo_badge_logo { margin-top:10px; width: 57px; height: 16px; }
         .vimeoBadge .credit { font: normal 11px verdana,sans-serif; }
         .vimeoBadge .clip { padding:0; float:left; margin:0 10px 10px 0; line-height:0; }
         .vimeoBadge.vertical .clip { float: none; }
-        .vimeoBadge .caption { font: normal 11px verdana,sans-serif; overflow:hidden; width: auto; height: 30px; }
+        .vimeoBadge .caption {line-height:1em; width: auto; font-size:0.8em; margin-top:5px; min-height:40px; }
         .vimeoBadge .clear { display: block; clear: both; visibility: hidden; }
         .vimeoBadge .s160 { width: 160px; } .vimeoBadge .s80 { width: 80px; } .vimeoBadge .s100 { width: 100px; } .vimeoBadge .s200 { width: 200px; }
-.vimeoClear{clear:both;}
+        .vimeoBadge .s160 img{ width: 160px; height: 120px; }
+		.vimeoClear{ clear:both; margin-bottom:40px;}
     </style>';
 
     if(!empty( $channel )){
-    	$retour .= '<h3><a target="_blank" href="'.$channelInfo->url.'">'.$channelInfo->name.'</h3></a>';
-    	$retour .= '<p>'.$channelInfo->description.'</p>';
+    	if(!isset( $notitle )){
+    		$retour .= '<h3><a target="_blank" href="'.$channelInfo->url.'">'.$channelInfo->name.'</h3></a>';
+		}
+		if(!isset( $nodesc )){
+    		$retour .= '<p>'.$channelInfo->description.'</p>';
+    	}
     	
     }
 
     $retour .= '<p>&nbsp;</p>';
-    $retour .= '<p><div id="badge">
+    $retour .= '<p><div class="badge">
 <div class="vimeoBadge horizontal">
 <script src="'.$url.'"></script>
 </div>
